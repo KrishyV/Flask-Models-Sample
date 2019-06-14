359261766ef05c67f5ced80f843cb7847ab96256
KrishyV <46026328+KrishyV@users.noreply.github.com>
2019-06-15 01:13:41 +0800

    Update models.py

--- models.py
+++ models.py
@@ -26,3 +26,22 @@ class Categories(db.Model):
  
  #   def __init__(self, **kwargs):
  #      super(Categories, self).__init__(**kwargs)
+    
+class Color(db.Model):
+    __tablename__ = 'color'
+    color_design = db.Column(db.String, unique=True, nullable=False)
+    id = db.Column(db.String(4), db.CheckConstraint('length(id) = 4'), unique=True, nullable=False)
+    type = db.Column(db.Integer, db.CheckConstraint('length(type) = 1' and 'type < 4'), unique=False, nullable=False)
+    pid = db.Column(db.Integer, primary_key=True, nullable=False)
+
+    # Some of the way you can implement constraints. For column type, I specified the integer can only
+    # be of 'length 1' and below 4 so essentially 0-3.
+    
+    def __repr__(self): # In case you are wondering, this is necessary for you to be able to display them.
+        return f"Color('{self.color_design}', '{self.id}', '{self.pid}', '{self.type}')"
+
+    def __init__(self, color_design, id, pid, type):
+        self.color_design = color_design
+        self.id = id
+        self.pid = pid
+        self.type = type
